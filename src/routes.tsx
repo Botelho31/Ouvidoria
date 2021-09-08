2b1fd42ed5de1fdde5c81118cbd76599faf6f14b
Lucas Botelho <lucas.vbotelho83@gmail.com>
2021-09-08 10:27:01 -0300

    feat: setup router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,24 @@
 import React, {FC} from 'react';
 import {NativeRouter, Route} from 'react-router-native';
-
-const Homepage = React.lazy(() => import('./views/homepage/homepage'));
+import Category from './views/category/category';
+import Complaint from './views/complaint/complaint';
+import Homepage from './views/homepage/homepage';
+import Login from './views/login/login';
+import News from './views/news/news';
+import Operation from './views/operation/operation';
+import Profile from './views/profile/profile';
+import Signup from './views/signup/signup';
 
 const Routes: FC = () => {
   return (
     <NativeRouter>
+      <Route path="/category" component={Category} />
+      <Route path="/complaint" component={Complaint} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/operation" component={Operation} />
+      <Route path="/news" component={News} />
       <Route exact path="/" component={Homepage} />
     </NativeRouter>
   );
