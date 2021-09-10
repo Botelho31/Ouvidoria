e4a8043cd1c17679deeed2223c005e0f912fd796
Lucas Botelho <lucas.vbotelho83@gmail.com>
2021-09-10 16:18:53 -0300

    fix: fc error

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard'
+    'standard',
+    'plugin:@typescript-eslint/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
