929b25d9cb0aec42350625c745f1aeef3dd0447c
Lucas Mendonça <lucasmmamaral@gmail.com>
2021-09-10 20:14:55 -0300

    refactor: change to clickable View

--- src/components/Buttons/primary-button.tsx
+++ src/components/Buttons/primary-button.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react'
+import { TouchableOpacity } from 'react-native'
 import styled from 'styled-components'
-import { Flexbox, Header4, StyleColors } from '../../styles'
+import { Header4, StyleColors } from '../../styles'
 interface BackgroundProps{
   width: number
 }
 
-const BackgroundFooter = styled(Flexbox)<BackgroundProps>`
+const BackgroundFooter = styled(TouchableOpacity)<BackgroundProps>`
   width: ${props => props.width}px;
   height: 32px;
   background-color: ${StyleColors.primary};
--- src/components/Buttons/secondary-button.tsx
+++ src/components/Buttons/secondary-button.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react'
+import { TouchableOpacity } from 'react-native'
 import styled from 'styled-components'
-import { Flexbox, Header3, StyleColors } from '../../styles'
+import { Header3, StyleColors } from '../../styles'
 interface BackgroundProps{
   width: number
 }
 
-const BackgroundFooter = styled(Flexbox)<BackgroundProps>`
+const BackgroundFooter = styled(TouchableOpacity)<BackgroundProps>`
   width: ${props => props.width}px;
   height: 32px;
   background-color: ${StyleColors.lightGray};
